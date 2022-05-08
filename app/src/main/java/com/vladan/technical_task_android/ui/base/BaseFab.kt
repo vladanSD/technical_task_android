c226c5cdf8a14733cc333bb9cba67474f011fb13
Vladan Jovanovic <vladan.jovanovic@bstorm.co>
2022-05-08 21:24:30 +0200

    added fab

---
+++ app/src/main/java/com/vladan/technical_task_android/ui/base/BaseFab.kt
@@ -0,0 +1,22 @@
+package com.vladan.technical_task_android.ui.base
+
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.vladan.technical_task_android.R
+import com.vladan.technical_task_android.ui.theme.primaryColor
+
+@ExperimentalComposeUiApi
+@Composable
+fun BaseFab(onClick: () -> Unit) {
+
+    FloatingActionButton(
+        onClick = { onClick() },
+        backgroundColor = primaryColor
+    ) {
+        Icon(painter = painterResource(id = R.drawable.ic_add_24), contentDescription = null, tint = Color.White)
+    }
+}
--- app/src/main/java/com/vladan/technical_task_android/ui/home/Home.kt
+++ app/src/main/java/com/vladan/technical_task_android/ui/home/Home.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FabPosition
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.vladan.technical_task_android.R
+import com.vladan.technical_task_android.ui.base.BaseFab
 import com.vladan.technical_task_android.ui.base.BaseProgressBar
 import com.vladan.technical_task_android.ui.base.BaseTopBar
 
@@ -29,8 +31,11 @@ fun Home(
     val state = vm.uiState.value
 
     Scaffold(
-        topBar = { BaseTopBar(title = R.string.home_screen_title, actions = { if (state.requestInProgress) BaseProgressBar() }) }) {
-        LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(vertical = 20.dp)) {
+        topBar = { BaseTopBar(title = R.string.home_screen_title, actions = { if (state.requestInProgress) BaseProgressBar() }) },
+        floatingActionButton = { BaseFab {} },
+        floatingActionButtonPosition = FabPosition.End
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(top = 20.dp, bottom = 80.dp)) {
             items(state.users, key = { it.id }) {
                 UserHolder(user = it, onLongPress = { Log.i("OVDE", it.name) }, modifier = Modifier)
             }
