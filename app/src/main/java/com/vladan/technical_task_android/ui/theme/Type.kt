a831a15fee3ab327dd32abd8bfcd7da11ddc8c1a
Vladan Jovanovic <vladan.jovanovic@bstorm.co>
2022-05-08 15:18:12 +0200

    fonts and typography

--- app/src/main/java/com/vladan/technical_task_android/ui/theme/Type.kt
+++ app/src/main/java/com/vladan/technical_task_android/ui/theme/Type.kt
@@ -2,27 +2,28 @@ package com.vladan.technical_task_android.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.vladan.technical_task_android.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+    Font(R.font.inter_regular, weight = FontWeight.Normal),
+    Font(R.font.inter_medium, weight = FontWeight.Medium)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = primaryColor
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    body2 = TextStyle(
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 14.sp,
+        color = grayColorEmail
     )
-    */
 )
